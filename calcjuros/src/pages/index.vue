8fd5a55c5c3f6acfeb469df54399c335f955c150
fox <fernandodiego.sn@gmail.com>
2024-08-09 18:20:01 -0300

    starting the project

---
+++ calcjuros/src/pages/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <HelloWorld></HelloWorld>
+  <AppFooter></AppFooter>
+</template>
+
+<script setup>
+//
+import AppFooter from "@/components/AppFooter.vue";
+import HelloWorld from "@/components/HelloWorld.vue";
+</script>
